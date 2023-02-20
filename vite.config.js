3e188f052010fa2f4e39bc88456a93404dbf7bce
Yurii Tymchuk <ytymchuck@seekingsimple.com>
2023-02-20 19:06:11 +0000

    Fix production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    build: {
+        target: 'esnext',
+    },
     server: {
         hmr: {
             host: 'localhost',
