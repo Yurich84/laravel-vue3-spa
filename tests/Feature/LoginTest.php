2eb5d27a660fd4b84ec1b6912b842f01bb602de8
Yurich <Yurich84@gmail.com>
2023-03-30 23:11:26 +0100

    Make core tests with pest

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -1,75 +1,63 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\PersonalAccessToken;
 use App\Models\User;
 use Illuminate\Support\Facades\Config;
-use Tests\TestCase;
 
-class LoginTest extends TestCase
-{
-    /** @test */
-    public function authenticate()
-    {
-        $response = $this->postJson(route('login'), [
-            'email' => $this->user->email,
-            'password' => 'password',
-            'device_name' => 'spa',
-        ]);
+test('authenticate', function () {
+    $response = $this->postJson(route('login'), [
+        'email' => $this->user->email,
+        'password' => 'password',
+        'device_name' => 'spa',
+    ]);
 
-        $token = $response->json()['token'];
+    $token = $response->json()['token'];
 
-        $response
-            ->assertSuccessful()
-            ->assertJson(['token' => $token])
-            ->assertHeader('Authorization');
+    $response
+        ->assertSuccessful()
+        ->assertJson(['token' => $token])
+        ->assertHeader('Authorization');
 
-        $this->withToken($token)
-            ->postJson(route('me'))
-            ->assertSuccessful();
+    $this->withToken($token)
+        ->postJson(route('me'))
+        ->assertSuccessful();
 
-        $this->assertDatabaseHas(PersonalAccessToken::TABLE_NAME, [
-            PersonalAccessToken::COLUMN_NAME => 'spa',
-            PersonalAccessToken::COLUMN_TOKENABLE_ID => $this->user->id,
-            PersonalAccessToken::COLUMN_TOKENABLE_TYPE => User::class,
-        ]);
-    }
+    $this->assertDatabaseHas(PersonalAccessToken::TABLE_NAME, [
+        PersonalAccessToken::COLUMN_NAME => 'spa',
+        PersonalAccessToken::COLUMN_TOKENABLE_ID => $this->user->id,
+        PersonalAccessToken::COLUMN_TOKENABLE_TYPE => User::class,
+    ]);
+});
 
-    /** @test */
-    public function fetch_the_current_user()
-    {
-        $this->actingAs($this->user)
-            ->postJson(route('me'))
-            ->assertSuccessful()
-            ->assertJsonPath('data.email', $this->user->email);
-    }
+test('fetch the current user', function () {
+    $this->actingAs($this->user)
+        ->postJson(route('me'))
+        ->assertSuccessful()
+        ->assertJsonPath('data.email', $this->user->email);
+});
 
-    /** @test */
-    public function log_out()
-    {
-        Config::set('auth.defaults.guard', 'api');
+test('log out', function () {
+    Config::set('auth.defaults.guard', 'api');
 
-        $response = $this->postJson(route('login'), [
-            'email' => $this->user->email,
-            'password' => 'password',
-            'device_name' => 'spa',
-        ]);
+    $response = $this->postJson(route('login'), [
+        'email' => $this->user->email,
+        'password' => 'password',
+        'device_name' => 'spa',
+    ]);
 
-        $token = $response->json()['token'];
+    $token = $response->json()['token'];
 
-        $this->withToken($token)
-            ->postJson(route('logout'))
-            ->assertOk();
+    $this->withToken($token)
+        ->postJson(route('logout'))
+        ->assertOk();
 
-        $this->assertDatabaseMissing(PersonalAccessToken::TABLE_NAME, [
-            PersonalAccessToken::COLUMN_NAME => 'spa',
-            PersonalAccessToken::COLUMN_TOKENABLE_ID => $this->user->id,
-            PersonalAccessToken::COLUMN_TOKENABLE_TYPE => User::class,
-        ]);
+    $this->assertDatabaseMissing(PersonalAccessToken::TABLE_NAME, [
+        PersonalAccessToken::COLUMN_NAME => 'spa',
+        PersonalAccessToken::COLUMN_TOKENABLE_ID => $this->user->id,
+        PersonalAccessToken::COLUMN_TOKENABLE_TYPE => User::class,
+    ]);
 
-        $this->withToken($token)
-            ->postJson(route('me'))
-            ->assertStatus(401);
-    }
-}
+    $this->withToken($token)
+        ->postJson(route('me'))
+        ->assertStatus(401);
+});
--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -1,27 +1,19 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Http\Controllers\Controller;
-use Tests\TestCase;
 
-class SettingsTest extends TestCase
-{
-    /** @test */
-    public function update_profile_info()
-    {
-        $this->actingAs($this->user)
-            ->patchJson(route('profile.update'), [
-                'name' => 'Test User',
-                'email' => 'test@test.app',
-            ])
-            ->assertSuccessful()
-            ->assertJson(['type' => Controller::RESPONSE_TYPE_SUCCESS]);
-
-        $this->assertDatabaseHas('users', [
-            'id' => $this->user->id,
+test('update profile info', function () {
+    $this->actingAs($this->user)
+        ->patchJson(route('profile.update'), [
             'name' => 'Test User',
             'email' => 'test@test.app',
-        ]);
-    }
-}
+        ])
+        ->assertSuccessful()
+        ->assertJson(['type' => Controller::RESPONSE_TYPE_SUCCESS]);
+
+    $this->assertDatabaseHas('users', [
+        'id' => $this->user->id,
+        'name' => 'Test User',
+        'email' => 'test@test.app',
+    ]);
+});
